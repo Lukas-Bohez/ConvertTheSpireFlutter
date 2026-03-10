2077de2f14729ca66bc896e2eace318789cbfe29
Lukas-Bohez <lukas.bohez@student.howest.be>
2026-03-10 10:32:55 +0100

    fix: restore flutter_inappwebview_windows registration to resolve startup crash issue

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -10,15 +10,8 @@
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-    // Temporarily skip registering flutter_inappwebview_windows on Windows
-    // to avoid a startup crash in certain environments caused by DirectComposition
-    // / WebView2 initialization. This is a targeted workaround for release builds
-    // while a proper fix is applied in the plugin. Remove this bypass once the
-    // upstream plugin is fixed.
-#if 0
-    FlutterInappwebviewWindowsPluginCApiRegisterWithRegistrar(
-            registry->GetRegistrarForPlugin("FlutterInappwebviewWindowsPluginCApi"));
-#endif
+  FlutterInappwebviewWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterInappwebviewWindowsPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
