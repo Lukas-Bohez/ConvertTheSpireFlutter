3386217f96cbd0ca6607a5a36b108a4f926af5ca
Lukas-Bohez <lukas.bohez@student.howest.be>
2026-05-01 21:26:43 +0200

    feat: update version to 10.6.3 and increment version code in build.gradle.kts and pubspec.yaml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         minSdk = 24
         // Target API 35 to satisfy Play Console requirements
         targetSdk = 35
-        versionCode = 1062
-        versionName = "10.6.2"
+        versionCode = 1063
+        versionName = "10.6.3"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
         }
