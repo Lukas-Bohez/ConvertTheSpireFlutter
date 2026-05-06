bd880dd6e2f0d16314ddab44ce27300e008b57eb
Lukas-Bohez <lukas.bohez@student.howest.be>
2026-05-06 20:48:36 +0200

    chore(release): bump to v10.8.7+1087

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         minSdk = 24
         // Target API 35 to satisfy Play Console requirements
         targetSdk = 35
-        versionCode = 1086
-        versionName = "10.8.6"
+        versionCode = 1087
+        versionName = "10.8.7"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
         }
