b714395624eaa2c561e14a05b5dc74eb6648b904
Lukas-Bohez <lukas.bohez@student.howest.be>
2026-05-12 20:29:08 +0200

    Bump version to 11.2.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         minSdk = 24
         // Target API 35 to satisfy Play Console requirements
         targetSdk = 35
-        versionCode = 1126
-        versionName = "11.2.6"
+        versionCode = 1127
+        versionName = "11.2.7"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
         }
