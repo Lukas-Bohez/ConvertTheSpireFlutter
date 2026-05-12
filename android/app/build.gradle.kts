910ab171c8f5d747c2c8f3fa815424c03daa63c9
Lukas-Bohez <lukas.bohez@student.howest.be>
2026-05-12 21:37:33 +0200

    bump version to 11.2.9 and update version code to 1129

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         minSdk = 24
         // Target API 35 to satisfy Play Console requirements
         targetSdk = 35
-        versionCode = 1127
-        versionName = "11.2.7"
+        versionCode = 1129
+        versionName = "11.2.9"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
         }
