45ffe2331be91ba9cc039bf72b14c21376418b37
Lukas-Bohez <lukas.bohez@student.howest.be>
2026-05-13 14:27:20 +0200

    chore(release): bump v11.3.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         minSdk = 24
         // Target API 35 to satisfy Play Console requirements
         targetSdk = 35
-        versionCode = 1132
-        versionName = "11.3.2"
+        versionCode = 1133
+        versionName = "11.3.3"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
         }
